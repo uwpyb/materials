ddf03e96860d745c2541040059557b1ce883d89e
Ivana Kajić <ivana.kajic@gmail.com>
2016-01-30 00:00:25 -0500

    Day 2 project update

--- projects/plotting_temperatures.py
+++ projects/plotting_temperatures.py
@@ -1,31 +1,23 @@
-import requests
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/CAN.csv"
-print('Getting the data from:', url)
-resp = requests.get(url)
-rows = resp.text.split('\n')
-temperatures = np.loadtxt(rows, delimiter=',', skiprows=1)
+temperatures = np.loadtxt('CAN.csv', delimiter=',', skiprows=1)
 
-fig, ax = plt.subplots()
-ax.plot(temperatures[:, 0], temperatures[:, 1])
-ax.set_title('Average temperatures in Canada from 1901 to 2009')
-ax.set_xlabel('Year')
-ax.set_ylabel('Temperature in C')
-ax.set_xlim([1901, 2009])
+plt.figure()
+plt.plot(temperatures[:, 0], temperatures[:, 1])
+plt.title('Average temperatures in Canada from 1901 to 2009')
+plt.xlabel('Year')
+plt.ylabel('Temperature in C')
+plt.xlim([1901, 2009])
 
-url_hrv = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/HRV.csv"
-resp_hrv = requests.get(url_hrv)
-temperatures_hrv = np.loadtxt(resp_hrv.text.split('\n'), delimiter=',', skiprows=1)
-
-url_deu = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/cru/tas/year/DEU.csv"
-resp_deu = requests.get(url_deu)
-temperatures_deu = np.loadtxt(resp_deu.text.split('\n'), delimiter=',', skiprows=1)
+temperatures_deu = np.loadtxt('DEU.csv', delimiter=',', skiprows=1)
+temperatures_hrv = np.loadtxt('HRV.csv', delimiter=',', skiprows=1)
 
 fig, ax = plt.subplots(figsize=(10, 4))
-ax.plot(temperatures_hrv[:, 0], temperatures_hrv[:, 1], label='HRV', color='r', linewidth=2)
-ax.plot(temperatures_deu[:, 0], temperatures_deu[:, 1], label='DEU', color='b', linewidth=2)
+ax.plot(temperatures_hrv[:, 0], temperatures_hrv[:, 1], label='HRV', color='r',
+        linewidth=2)
+ax.plot(temperatures_deu[:, 0], temperatures_deu[:, 1], label='DEU', color='b',
+        linewidth=2)
 ax.set_title('Average temperatures from 1901 to 2009')
 ax.set_xlabel('Year')
 ax.set_ylabel('Temperature in C')
@@ -33,6 +25,9 @@ ax.set_xlim([1901, 2009])
 
 plt.legend(loc='best')
 plt.grid('on')
-
 plt.savefig('temperatures_hrv_deu.pdf')
+
+
 plt.show()
+
+
